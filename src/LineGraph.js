2fd8f4ebfa00ff45e1027920bf642e01e8718e8d
Maya Kano <mayakanoweb@gmail.com>
2021-11-09 20:27:01 -0500

    installed linegraph library for chart functionality

---
+++ src/LineGraph.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Line} from "react-chartjs-2";
+
+function LineGraph() {
+
+    const data = 
+    [{
+        x:10,
+        y:20
+    }, {
+        x:15,
+        y:10
+    }]
+    return (
+        <div className="linegraph">
+            <Line 
+                data={{
+                    datasets: [
+                        {
+                            type: "line",
+                            data: data
+                        }
+                    ]
+                
+                }}
+                
+            />
+        </div>
+    )
+}
+
+export default LineGraph
