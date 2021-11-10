44fd92b695f4b46a94a96ad1a66c18de49898234
Maya Kano <mayakanoweb@gmail.com>
2021-11-10 10:08:53 -0500

    linear chart setup

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import {Line} from "react-chartjs-2";
+import {Line} from 'react-chartjs-2';
+
 
 function LineGraph() {
+
 
     const data = 
     [{
@@ -10,20 +12,54 @@ function LineGraph() {
     }, {
         x:15,
         y:10
+    }, {
+        x:12,
+        y:4
     }]
     return (
         <div className="linegraph">
             <Line 
-                data={{
+                data= {{
+                    labels: ["Nausea", "Fever", "Muscle Pain"],
                     datasets: [
-                        {
+                        { 
                             type: "line",
-                            data: data
+                            label: 'First dataset',
+                            // axis: 'y',
+                            // fill: false,
+                            // tension: 0.1,
+                            backgroundColor: "black",
+                            borderColor: "#5AC53B",
+                            borderWidth: 2,
+                            pointBorderColor: 'rgba(0, 0, 0, 0)',
+                            pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+                            pointHoverBackgroundColor: '#5AC53B',
+                            pointHoverBorderColor: '#000000',
+                            pointHoverBorderWidth: 4,
+                            pointHoverRadius: 6,
+                            data: data,
                         }
                     ]
-                
+    
                 }}
-                
+                options={{
+                    legend: {
+                        display: false               
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                display: false,
+
+                                // beginAtZero: true
+                            // },
+                            // y: {
+                            //     suggestedMin: 50,
+                            //     suggestedMax: 100
+                            }
+                        }],
+                    },
+                }}
             />
         </div>
     )
